d81c7deff7aca7c5968552775ef9d78e54e8c73e
pandafulmanda <pandafulmanda@gmail.com>
2015-09-12 04:03:16 -0500

    lesson two project is set up

--- lesson-two/exercise/assets/main.js
+++ lesson-two/exercise/assets/main.js
@@ -6,3 +6,16 @@ var robot = new Robot(robotElement);
 var animator = new Animator();
 
 animator.startLoop();
+
+sceneElement.addEventListener('mousemove', handleMouseMove);
+
+function handleMouseMove(mouseEvent){
+  // The robot's position -- we can update the direction and angle
+  var robotPosition = robot.getPosition();
+  // We only need the mouse position. We can leave out all other information about
+  // the how the mouse moves and only tell watchMouse the x and y of the mouse.
+  var mousePosition = _.pick(mouseEvent, 'x', 'y');
+
+  // This is where watchMouse gets called.
+  watchMouse(robotPosition, mousePosition);
+}
--- lesson-two/project/assets/main.js
+++ lesson-two/project/assets/main.js
@@ -1,1 +1,33 @@
-// initialize project here
+var robotElement = document.getElementById('robot');
+var sceneElement = document.getElementById('scene');
+
+var scene = new Scene(sceneElement);
+var robot = new Robot(robotElement);
+var animator = new Animator();
+
+animator.startLoop();
+
+sceneElement.addEventListener('mousemove', handleMouseMove);
+sceneElement.addEventListener('click', handleClick);
+
+function handleMouseMove(mouseEvent){
+  // The robot's position -- we can update the direction and angle
+  var robotPosition = robot.getPosition();
+  // We only need the mouse position. We can leave out all other information about
+  // the how the mouse moves and only tell watchMouse the x and y of the mouse.
+  var mousePosition = _.pick(mouseEvent, 'x', 'y');
+
+  // This is where watchMouse gets called.
+  watchMouse(robotPosition, mousePosition);
+}
+
+function handleClick(mouseEvent){
+  // The robot's position -- we can update the direction and angle
+  var robotPosition = robot.getPosition();
+  // We only need the mouse position. We can leave out all other information about
+  // the how the mouse moves and only tell moveRobot the x and y of the mouse.
+  var mousePosition = _.pick(mouseEvent, 'x', 'y');
+
+  // This is where moveRobot gets called.
+  moveRobot(robotPosition, mousePosition);
+}
