9800cf98764896d3a727db485e10ebeb27162be4
pandafulmanda <pandafulmanda@gmail.com>
2015-10-05 08:27:45 -0500

    update example to current

--- assets/example.js
+++ assets/example.js
@@ -1,17 +1,18 @@
 
-var myNumbers = [10, 20, 80, 100, 120, 143];
+var myNumbers = [10, 20, 80, 100, 212];
 
-function calculateSum(numbersList){
-  // start sum at 0
+// var sum = myNumbers[0] + myNumbers[1] + myNumbers[2] + myNumbers[3] + myNumbers[4]
+// var sum = 10 + 20 + 80 + 100 + 212
+
+function calculateSum(listOfNumbers){
   var sum = 0;
 
-  // For each number in numbersList,
-  numbersList.forEach(function(number){
-    // add the number to the sum
-    // and update the sum.
-    sum += number;
-  });
+  listOfNumbers.forEach(
+    //do something for each item
+    function (number){
+      sum = sum + number;
+    }
+  )
 
-  // Return the sum out.
   return sum;
 }
